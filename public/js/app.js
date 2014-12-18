362bcb9e5b9e51820e8cef5345ac719f83229ce9
Jason Lee <chsienlee@gmail.com>
2014-12-19 02:41:29 +0800

    ahrs

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,31 @@
-var locateApp = angular.module('locateApp', ['ngAnimate', 'angularMoment', 'btford.socket-io']);
-locateApp.run(function(amMoment) {
-  amMoment.changeLanguage('zh-tw');
-});
+(function() {
+  var AHRSApp = angular.module('AHRSApp', ['ngAnimate', 'angularMoment', 'btford.socket-io']);
 
-locateApp.factory('socket', function (socketFactory) {
-  return socketFactory();
-});
-
-locateApp.controller('locateCtrl', function ($scope, $http, socket) {
-  $scope.beacons = [];
-  $scope.postion = null;
-
-  $scope.update_beacons = function(beacon) {
-    for (var i = 0; i < $scope.beacons.length; i++)
-      if ($scope.beacons[i].uuid == beacon.uuid) {
-        $scope.beacons[i].uuid = beacon.uuid;
-        $scope.beacons[i].accuracy = beacon.accuracy;
-        $scope.beacons[i].updated_at = beacon.updated_at;
-        return;
-      }
-    if (beacon.accuracy != -1)
-      $scope.beacons.push(beacon);
-  };
-
-  $scope.update_beacon_axis = function(beacon) {
-    socket.emit('updateBeaconAxis', beacon);
-    $scope.update_axis_chart();
-  };
-
-  $scope.update_axis_chart = function() {
-    var board = JXG.JSXGraph.initBoard('box', {boundingbox: [-100, 3000, 3000, -200], axis:true});
-    for (var i = 0; i < $scope.beacons.length; i++) {
-      if ($scope.beacons[i].x != -1 && $scope.beacons[i].y != -1) {
-        var p = board.create('point', [$scope.beacons[i].x*100, $scope.beacons[i].y*100], {color: 'blue'});
-      }
-    }
-    var p = board.create('point', [$scope.position.x*100, $scope.position.y*100], {color: 'red'});
-  };
-
-  socket.on('updateBeacon', function (beacon) {
-    $scope.update_beacons(beacon);
+  AHRSApp.run(function(amMoment) {
+    amMoment.changeLanguage('zh-tw');
   });
 
-  socket.on('updateLocation', function (location) {
-//    console.log(beacon.uuid + ' update axis');
-    $scope.position = location.pos;
-    $scope.update_axis_chart();
+  AHRSApp.factory('socket', function (socketFactory) {
+    return socketFactory();
   });
-});
+
+  AHRSApp.controller('AHRSCtrl', function ($scope, $http, socket) {
+    $scope.ahrs_data = {
+      pitch:  0,
+      yaw:    0,
+      roll:   0,
+      accelX: 0,
+      accelY: 0,
+      accelZ: 0,
+      gyroX:  0,
+      gyroY:  0,
+      gyroZ:  0
+    };
+
+    socket.on('updateAHRS', function (ahrs_data) {
+      for (key in ahrs_data) {
+        $scope.ahrs_data[key] = ahrs_data[key];
+      }
+    });
+  });
+})();
